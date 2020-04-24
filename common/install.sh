6f17be771ff8873d7faec2c03141af1ab532e12d
E7KMbb <34418195+E7KMbb@users.noreply.github.com>
2020-04-24 09:34:59 +0800

    Add files via upload
    新方案

--- common/install.sh
+++ common/install.sh
@@ -1,34 +1,10 @@
-if [ -e /system/product/media/bootanimation.zip ]; then
-#一般设备Android10开机目录
-  mv $MODPATH/bootanimation.zip $MODPATH/system/product/media
-  set_perm $MODPATH/system/product/media/bootanimation.zip 0 0 0644 u:object_r:system_file:s0
-  else
-  if [ -e /system/media/bootanimation.zip ]; then
-  #一般设备Android10以下开机动画目录
-    mv $MODPATH/bootanimation.zip $MODPATH/system/media
-    set_perm $MODPATH/system/media/bootanimation.zip 0 0 0644 u:object_r:system_file:s0
-    else
-    if [ -e /system/media/theme/bootup/bootanimation.zip ]; then
-    #努比亚开机动画目录
-      mv $MODPATH/bootanimation.zip $MODPATH/system/media/theme/bootup
-      set_perm $MODPATH/system/media/theme/bootup/bootanimation.zip 0 0 0644 u:object_r:system_file:s0
-        else
-          if [ -e /system/media/bootanimation.qmg ]; then
-          #三星开机动画目录
-            mv $MODPATH/bootanimation.qmg $MODPATH/system/media
-            set_perm $MODPATH/system/media/bootanimation.qmg 0 0 0644 u:object_r:system_file:s0
-            else
-             if [ -e /system/media/bootanimation/bootanimation.zip ]; then
-             #OPPO开机动画目录
-             mv $MODPATH/bootanimation.zip $MODPATH/system/media/bootanimation
-             set_perm $MODPATH/system/media/bootanimation/bootanimation.zip 0 0 0644 u:object_r:system_file:s0
-             else
-             ui_print "不支持你的设备！"
-             exit 1
-           fi
-        fi
-      fi
-   fi
+find /system -name bootanimation.* > $MODPATH/directory.txt
+sed -i 's/\/bootanimation.*//' $MODPATH/directory.txt
+directory=$(cat $MODPATH/directory.txt)
+if [ -s $MODPATH/directory.txt ]; then
+mkdir -p $MODPATH$directory
+mv $MODPATH/bootanimation.* $MODPATH$directory
+else
+abort "您的开机动画文件不存在于Magisk的工作目录"
 fi
-rm -rf \
-$MODPATH/system/product/media/Placeholder $MODPATH/system/media/Placeholder $MODPATH/system/media/theme/bootup/Placeholder $MODPATH/system/media/bootanimation/Placeholder $MODPATH/product \
+rm -rf $MODPATH/directory.txt
