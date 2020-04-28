e4a790b1e6169e19c3eedf02725889dfc224541f
E7KMbb <34418195+E7KMbb@users.noreply.github.com>
2020-04-28 16:36:55 +0800

    Add files via upload
    添加对dark模式开机动画的支持，添加英文注释

--- common/install.sh
+++ common/install.sh
@@ -1,10 +1,12 @@
 find /system -name bootanimation.* > $MODPATH/directory.txt
 sed -i 's/\/bootanimation.*//' $MODPATH/directory.txt
-directory=$(sed -n '1p' $MODPATH/directory.txt)
+directory=$(cat $MODPATH/directory.txt)
 if [ -s $MODPATH/directory.txt ]; then
 mkdir -p $MODPATH$directory
 mv $MODPATH/bootanimation.* $MODPATH$directory
+mv $MODPATH/bootanimation-dark.* $MODPATH$directory
 else
+ui_print "Your bootanimation file does not exist in Magisk's working directory"
 abort "您的开机动画文件不存在于Magisk的工作目录"
 fi
 rm -rf $MODPATH/directory.txt
